18c12d83dd0321a56c3ef8cd5de531a8dcae37b3
Yadier Martinez <yadieralexander19@gmail.com>
2023-07-05 19:37:05 -0400

    First release

---
+++ src/features/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import CartOverview from "./../cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+export default function AppLayout() {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
+      {isLoading && <Loader />}
+      <Header />
+      <div className="no-scrollbar overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
+}
